e061e13476d63265f486ea3a0089f6a670e33fe7
Joris Deguet <joris.deguet@gmail.com>
2022-04-11 11:42:10 -0400

    Fix comportement insert null sur auto increment

--- src/main/java/org/sbac/ConfigurationSourceDonnees.java
+++ src/main/java/org/sbac/ConfigurationSourceDonnees.java
@@ -17,7 +17,7 @@ public class ConfigurationSourceDonnees {
     public DataSource getDataSource() {
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
         dataSourceBuilder.driverClassName("org.h2.Driver");
-        dataSourceBuilder.url("jdbc:h2:mem:test");
+        dataSourceBuilder.url("jdbc:h2:mem:test;MODE=LEGACY");
         dataSourceBuilder.username("SA");
         dataSourceBuilder.password("");
         return dataSourceBuilder
